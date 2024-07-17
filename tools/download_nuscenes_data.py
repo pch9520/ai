08ff5c375cc16515a9b799d0f6e94977a1c44177
pch9520 <pch9520@qq.com>
2024-07-18 00:44:10 +0800

    add nuscenes download tool

---
+++ tools/download_nuscenes_data.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import requests
+import os
+import hashlib
+from tqdm import tqdm
+import tarfile
+import gzip
+import json
+
+from retrying import retry
+
+
+# replace your email and password in https://www.nuscenes.org/
+useremail = os.getenv("nuscenes_user_email", "")
+password = os.getenv("nuscenes_password", "")
+assert useremail != "", "lack user email!!!"
+assert password != "", "lack password!!!"
+
+output_dir = "/Volumes/PS2000W/data/nuscenes"
+region = 'asia'  # 'us' or 'asia'
+
+
+download_files = {
+    "v1.0-test_meta.tgz": "b0263f5c41b780a5a10ede2da99539eb",
+    "v1.0-test_blobs.tgz": "e065445b6019ecc15c70ad9d99c47b33",
+    "v1.0-trainval01_blobs.tgz": "cbf32d2ea6996fc599b32f724e7ce8f2",
+    "v1.0-trainval02_blobs.tgz": "aeecea4878ec3831d316b382bb2f72da",
+    "v1.0-trainval03_blobs.tgz": "595c29528351060f94c935e3aaf7b995",
+    "v1.0-trainval04_blobs.tgz": "b55eae9b4aa786b478858a3fc92fb72d",
+    "v1.0-trainval05_blobs.tgz": "1c815ed607a11be7446dcd4ba0e71ed0",
+    "v1.0-trainval06_blobs.tgz": "7273eeea36e712be290472859063a678",
+    "v1.0-trainval07_blobs.tgz": "46674d2b2b852b7a857d2c9a87fc755f",
+    "v1.0-trainval08_blobs.tgz": "37524bd4edee2ab99678909334313adf",
+    "v1.0-trainval09_blobs.tgz": "a7fcd6d9c0934e4052005aa0b84615c0",
+    "v1.0-trainval10_blobs.tgz": "31e795f2c13f62533c727119b822d739",
+    "v1.0-trainval_meta.tgz": "537d3954ec34e5bcb89a35d4f6fb0d4a",
+}
+
+
+def login(username, password):
+    headers = {
+        "content-type": "application/x-amz-json-1.1",
+        "x-amz-target": "AWSCognitoIdentityProviderService.InitiateAuth",
+    }
+
+    data = (
+        '{"AuthFlow":"USER_PASSWORD_AUTH","ClientId":"7fq5jvs5ffs1c50hd3toobb3b9","AuthParameters":{"USERNAME":"'
+        + username
+        + '","PASSWORD":"'
+        + password
+        + '"},"ClientMetadata":{}}'
+    )
+
+    response = requests.post(
+        "https://cognito-idp.us-east-1.amazonaws.com/",
+        headers=headers,
+        data=data,
+    )
+
+    token = json.loads(response.content)["AuthenticationResult"]["IdToken"]
+
+    return token
+
+
+@retry(stop_max_attempt_number=3, wait_fixed=1000)
+def download_file(url, save_file, md5):
+    response = requests.get(url, stream=True)
+    if save_file.endswith(".tgz"):
+        content_type = response.headers.get('Content-Type', '')
+        if content_type == 'application/x-tar':
+            save_file = save_file.replace('.tgz', '.tar')
+        elif content_type != 'application/octet-stream':
+            print("unknow content type", content_type)
+            return save_file
+
+    if os.path.exists(save_file):
+        print(save_file, "has downloaded")
+        # check md5
+        md5obj = hashlib.md5()
+        with open(save_file, 'rb') as file:
+            for chunk in file:
+                md5obj.update(chunk)
+        hash = md5obj.hexdigest()
+        if hash != md5:
+            print(save_file, "check md5 failed,download again")
+        else:
+            print(save_file, "check md5 success")
+            return save_file
+
+    file_size = int(response.headers.get('Content-Length', 0))
+    progress_bar = tqdm(total=file_size, unit='B',
+                        unit_scale=True, unit_divisor=1024, desc=save_file, ascii=True)
+
+    # save file & check md5
+    md5obj = hashlib.md5()
+    with open(save_file, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                md5obj.update(chunk)
+                file.write(chunk)
+                progress_bar.update(len(chunk))
+    progress_bar.close()
+
+    hash = md5obj.hexdigest()
+    assert hash == md5, f"md5 check failed, {save_file}"
+    print(f"{save_file} check md5 success")
+
+    return save_file
+
+
+def extract_tgz_to_original_folder(tgz_file_path):
+    original_folder = os.path.dirname(tgz_file_path)
+    tqdm.write(f"Extracting {tgz_file_path} to {original_folder}")
+
+    with gzip.open(tgz_file_path, 'rb') as f_in:
+        with tarfile.open(fileobj=f_in, mode='r') as tar:
+            for member in tqdm(iterable=tar.getmembers(), total=len(tar.getmembers()), desc=tgz_file_path):
+                tar.extract(member, path=original_folder)
+
+
+def extract_tar_to_original_folder(tar_file_path):
+    original_folder = os.path.dirname(tar_file_path)
+    tqdm.write(f"Extracting {tar_file_path} to {original_folder}")
+
+    with tarfile.open(tar_file_path, 'r') as tar:
+        for member in tqdm(iterable=tar.getmembers(), total=len(tar.getmembers()), desc=tar_file_path):
+            tar.extract(member, path=original_folder)
+
+
+def main():
+    print("Loginging...")
+    bearer_token = login(useremail, password)
+    # set request header
+    headers = {
+        'Authorization': f'Bearer {bearer_token}',
+        'Content-Type': 'application/json',
+    }
+
+    print("Getting download urls...")
+    download_data = {}
+    for filename, md5 in download_files.items():
+        api_url = f'https://o9k5xn5546.execute-api.us-east-1.amazonaws.com/v1/archives/v1.0/{filename}?region={region}&project=nuScenes'
+
+        response = requests.get(api_url, headers=headers)
+
+        if response.status_code == 200:
+            print(filename, 'request success')
+            download_url = response.json()['url']
+            download_data[filename] = [download_url, os.path.join(output_dir, filename), md5]
+        else:
+            print(f'request failed : {response.status_code}')
+            print(response.text)
+
+    print("Downloading files...")
+
+    os.makedirs(output_dir, exist_ok=True)
+    futures = []
+    input_params = []
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        for output_name, (download_url, save_file, md5) in download_data.items():
+            futures.append(executor.submit(download_file, download_url, save_file, md5))
+            input_params.append(output_name)
+    for idx, future in enumerate(as_completed(futures)):
+        save_file = future.result()
+        output_name = input_params[idx]
+        download_data[output_name][1] = save_file
+
+    print("Extracting files...")
+    futures.clear()
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for _, (download_url, save_file, md5) in download_data.items():
+            if save_file.endswith(".tgz"):
+                futures.append(executor.submit(extract_tgz_to_original_folder, save_file))
+            elif save_file.endswith(".tar"):
+                futures.append(executor.submit(extract_tar_to_original_folder, save_file))
+            else:
+                tqdm.write("unknow file type", save_file)
+        for future in as_completed(futures):
+            future.result()
+
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
